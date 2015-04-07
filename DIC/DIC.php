f4a280efcf4293297b0e5a1ccb7483adf159f9de
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-07 22:01:57 +0700

    Update Description

--- DIC/DIC.php
+++ DIC/DIC.php
@@ -1,34 +1,56 @@
 <?php
+/*///////////////////////////////////////////////////////////////
+ /** ID: | /-- ID: Indonesia
+ /** EN: | /-- EN: English
+ ///////////////////////////////////////////////////////////////*/
+
 /**
- * DIC - Library untuk framework kecik, library ini khusus untuk membantu masalah Dependency Injection 
- *
+ * ID: DIC - Library untuk Kecik Framework, library ini khusus untuk membantu masalah Dependency Injection 
+ * EN: DIC - Library for Kecik Framework, this library specially for help Dependency Injection Problem
+ * 
  * @author 		Dony Wahyu Isp
  * @copyright 	2015 Dony Wahyu Isp
- * @link 		http://github.io/kecik_dic
+ * @link 		http://github.com/kecik-framework/dic
  * @license		MIT
- * @version 	1.0-alpha
+ * @version 	1.0.1-alpha
  * @package		Kecik\DIC
  **/
 namespace Kecik;
 
 /**
- * Controller
+ * DIC
  * @package 	Kecik\DIC
  * @author 		Dony Wahyu Isp
- * @since 		1.0-alpha
+ * @since 		1.0.0-alpha
  **/
-
 class DIC implements \ArrayAccess{
+    /**
+     * @var stdclass $storage
+     **/
 	private $storage = null;
 
+    /**
+     * Constructor
+     **/
 	public function __construct() {
 		$this->storage = new \stdClass;
 	}
 
+    /**
+     * factory
+     * ID: Untuk selalu membuat instan baru dari sebuah objek
+     * EN: For always create new instance from a object
+     * @return array callback
+     **/
 	public function factory(\Closure $func) {
 		return array('factory'=>$func);
 	}
 
+    /**
+     * offsetSet
+     * ID: Untuk menampung atau menyetting fungsi baru ke dalam storage
+     * EN: For a collect or setting new function in storage
+     **/
 	public function offsetSet($offset, $value) {
         if (!empty($offset)) {
             $this->storage->$offset = $value;
@@ -36,14 +58,30 @@ class DIC implements \ArrayAccess{
 
     }
 
+    /**
+     * offsetExists
+     * ID: Jika sebuah funngsi telah didefinisi
+     * EN: If a function are exists
+     * @return function/closure ID: jika belum pernah digunakan | EN: if never use
+     **/
     public function offsetExists($offset) {
         return isset($this->storage->$offset);
     }
 
+    /**
+     * offsetUnset
+     * ID: Ketika fungsi dihapus
+     * EN: when function deleting
+     **/
     public function offsetUnset($offset) {
         unset($this->storage->$offset);
     }
 
+    /**
+     * offsetGet
+     * ID: Ketika fungsi digunakan
+     * EN: When using function
+     **/
     public function offsetGet($offset) {
     	$callable = $this->storage->$offset;
     	
